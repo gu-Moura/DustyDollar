1ce6657c203952c2dad296e066c2ce664f158033
Gustavo Moura <gustavopereira0@yahoo.com>
2023-12-19 07:58:45 -0300

    Add db_models for migrations and script to populate DB with fake data

---
+++ src/db_models.py
@@ -0,0 +1,26 @@
+from config import db
+
+
+class Conta(db.Model):
+    id_conta = db.Column(db.Integer, primary_key=True, nullable=False)
+    id_pessoa = db.Column(db.Integer, nullable=False)
+    senha = db.Column(db.Text, nullable=False)
+    saldo = db.Column(db.DECIMAL(10, 2), default=0.0)
+    limite_saque_diario = db.Column(db.DECIMAL(10, 2), default=1000.0)
+    flag_ativo = db.Column(db.Boolean, default=True)
+    tipo_conta = db.Column(db.Integer, nullable=False)
+    data_criacao = db.Column(db.Date, nullable=False)
+
+
+class Transacao(db.Model):
+    id_transacao = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    id_conta = db.Column(db.Integer, nullable=False)
+    valor = db.Column(db.DECIMAL(10, 2), nullable=False)
+    data_transacao = db.Column(db.Date, nullable=False)
+
+
+class Pessoa(db.Model):
+    id_pessoa = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    nome = db.Column(db.Text(), nullable=False)
+    cpf = db.Column(db.String(11), nullable=False)
+    data_nascimento = db.Column(db.Date, nullable=False)
--- src/fake_data_generator.py
+++ src/fake_data_generator.py
@@ -1,54 +1,61 @@
+from functools import wraps
+
 from faker import Faker
 import random
-from models.entities import Pessoa, Conta, Transacao
+from models.entities import Person, Account, Transaction
+import services.db_service as sqla
 
 fake = Faker("pt_BR")
 
 
-def generate_transaction(acc_id: int, transaction_id: int) -> Transacao:
+def generate_transaction(acc_id: int, transaction_id: int) -> Transaction:
     transaction = {
         'id_transacao': transaction_id,
         'id_conta': acc_id,
         'valor': random.random() * 10000 - 5000,
         'data_transacao': fake.date()
     }
-    return Transacao.from_dict(transaction)
+    return Transaction.from_dict(transaction)
 
 
-def generate_conta(id_pessoa: int = -1) -> Conta:
+def generate_conta(id_pessoa: int = -1) -> Account:
     account = {
         'id_conta': fake.aba(),
         'id_pessoa': id_pessoa,
         'saldo': random.random() * 10000 - 5000,
         'limite_saque_diario': random.random() * 10000,
-        'flag_ativo': random.random() > 0.5,
-        'tipo_conta': random.randint(0, 3),
+        'flag_ativo': True,
+        'tipo_conta': 1,
         'data_criacao': fake.date()
     }
-    return Conta.from_dict(account)
+    return Account.from_dict(account)
 
 
-def generate_person(pid: int) -> Pessoa:
+def generate_person(pid: int) -> Person:
     person = {
         "id_pessoa": pid,
         "nome": fake.name(),
-        "cpf": fake.cpf(),
+        "cpf": fake.cpf().replace('.', '').replace('-', ''),
         "data_nascimento": fake.date_of_birth().strftime("%Y-%m-%d")
     }
-    return Pessoa.from_dict(person)
-
+    return Person.from_dict(person)
 
 
 def populate_database():
-    pessoas = [generate_person(i) for i in range(125)]
+    pessoas = [generate_person(i + 1) for i in range(125)]
     contas = [generate_conta() for _ in range(250)]
     transacoes = [generate_transaction(acc_id=-1, transaction_id=i) for i in range(5000)]
 
+    for pessoa in pessoas:
+        sqla.create_new_person(pessoa)
+
     for conta in contas:
         conta.id_pessoa = random.choice(pessoas).id_pessoa
+        sqla.create_new_account(conta)
 
     for transacao in transacoes:
         transacao.id_conta = random.choice(contas).id_conta
+        sqla.make_transaction(transacao.id_conta, transacao.valor)
 
     print("Done")
 
