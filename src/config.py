b4ed4f56bc60922323ca2d280160d8d7694c20c4
Gustavo Moura <gustavopereira0@yahoo.com>
2023-12-19 07:58:45 -0300

    Add configs for JWT, bcrypt and DB

--- src/config.py
+++ src/config.py
@@ -2,9 +2,20 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
+from env_variables import DB_NAME, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, JWT_SECRET_KEY
+
+
+db_url = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://sherrif:maverick@localhost:3306/DustyDollar'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+jwt = JWTManager(app)
+bcrypt = Bcrypt(app)
+
